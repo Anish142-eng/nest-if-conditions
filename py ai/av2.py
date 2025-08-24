59d2841a5f27fd5397b989c3f3d36c21b1a6f597
Anish142-eng <kuchalkumar770@gmail.com>
2025-08-24 22:11:51 +0500

    Add files via upload

---
+++ py ai/av2.py
@@ -0,0 +1,17 @@
+i = int(input("Enter your prefrence Scooty.1 or Car.2: "))
+if(i == 1):
+    print("you have selected scooty ")
+    l = int(input("ymaha.1 or royal mk.2: "))
+    if(l == 1):
+        print("you have selected a ymaha ")
+    else:
+        print("you have selected royal mk ") 
+elif(i == 2):
+    print("you have selected car ")
+    l = int(input("BMW.1 or Mercedes.2: "))
+    if(l == 1):
+        print("you have selected a BMW ")
+    else:
+        print("you have selected Mercedes ") 
+
+       
